18b80f8ddc1b5fca12178d71885d36ae58527c81
Parker Kemp <charles.kemp702@topper.wku.edu>
2014-04-13 23:32:24 -0500

    Added a basic dialog box for a menu; also added a function to map keypresses to specific functions (ESC opens the menu dialog, up arrow moves up).

--- KeyEvents.m
+++ KeyEvents.m
@@ -6,6 +6,9 @@ BeginPackage["KeyEvents`"]
 initJLink::usage="Initializes JLink, along with the things necessary for key event handling."
 
 keyDown
+keyUp
+keyTyped
+moveUp
 
 initWindow::usage="Create the main window. Must be called after initJLink."
 
@@ -16,24 +19,58 @@ Begin["Private`"]
 keyDown[e_,char_,code_]:=Module[
 {},
 System`out@println["Key pressed."];
-Global`playerLocation[[2]]-=5;
+Print[FullForm[code]];
+mapKeyDown[code];
+]
+
+mapKeyDown[code_]:=Module[
+{},
+Switch[code,
+	KeyEvent`VKUESCAPE,KeyEvents`mainMenu[],
+	KeyEvent`VKUUP,KeyEvents`moveUp[]
+	]
+]
+
+moveUp[]:=Module[
+{},
+	System`out@println["Movin' on up!"];
+	Global`playerLocation[[2]] += 3;
+]
+
+mainMenu[]:=Module[
+{},
+	Switch[ChoiceDialog["Space Chase", {"New Game"->1,"Exit"->2}],
+		1,Print["New game chosen."],
+		2,Print["Exit chosen."]
+	]
+]
+
+keyUp[e_,char_,code_]:=Module[
+{},
+System`out@println["Key released."];
+]
+
+keyTyped[e_,char_,code_]:=Module[
+{},
+System`out@println["Key typed."];
 ]
 
 initJLink[]:=Module[
 {},
 ReinstallJava[];
 LoadJavaClass["java.lang.System"];
+LoadJavaClass["java.awt.event.KeyEvent"];
 ]
 
 initWindow[]:=Module[
 {},
 listenerClass=LoadJavaClass["com.wolfram.jlink.MathKeyListener"];
-listener=JavaNew[listenerClass,{{"keyPressed","KeyEvents`keyDown"},{"keyReleased","KeyEvents`keyDown"},{"keyTyped","KeyEvents`keyDown"}}];
+listener=JavaNew[listenerClass,{{"keyPressed","KeyEvents`keyDown"},{"keyReleased","KeyEvents`keyUp"},{"keyTyped","KeyEvents`keyTyped"}}];
 
 frame=JavaNew["com.wolfram.jlink.MathFrame"];
 frame@addKeyListener[listener];
 JavaShow[frame];
-(*ShowJavaConsole[];*)
+ShowJavaConsole[];
 ]
 
 drawPlayer[]:=Module[
